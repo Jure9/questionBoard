3a353fab4bfdb57b24f8a211893a4e8ae591e294
Jure Jakin <jure.jakin9@gmail.com>
2019-07-09 12:33:32 +0200

    Created vue answers component

--- resources/js/components/Answer.vue
+++ resources/js/components/Answer.vue
@@ -1,6 +1,47 @@
-<!--<template>-->
-<!---->
-<!--</template>-->
+<template>
+    <div class="media post">
+
+        <vote :model="answer" name="answer"></vote>
+
+        <div class="media-body">
+
+            <form v-if="editing" @submit.prevent="update">
+                <div class="form-group">
+                    <textarea rows="10" v-model="body" class="form-control" required>
+                    </textarea>
+                </div>
+                <button class="btn btn-primary" :disabled="isInvalid">Update</button>
+                <button class="btn btn-outline-secondary" @click="cancel" type="button">Cancel</button>
+            </form>
+
+            <div v-else>
+                <div v-html="bodyHtml"></div>
+                <div class="row">
+                    <div class="col-4">
+                        <div class="ml-auto">
+
+                            <a v-if="authorize('modify', answer)" @click.prevent="edit" class="btn btn-sm btn-outline-info">Edit</a>
+
+                            <button v-if="authorize('modify', answer)" @click="destroy" class="btn btn-sm btn-outline-danger">Delete</button>
+
+                        </div>
+                    </div>
+
+
+                    <div class="col-4">
+
+                    </div>
+
+                    <div class="col-4 float-right">
+                        <user-info :model="answer" label="Answered"></user-info>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
 
 <script>
     export default {
@@ -56,12 +97,12 @@
                     buttons: [
                         ['<button><b>YES</b></button>', (instance, toast) => {
 
-                                axios.delete(this.endpoint)
-                                    .then(res => {
-                                        $(this.$el).fadeOut(500, () => {
-                                            this.$toast.success(res.data.message, "Success", {timeout: 3000});
-                                        })
-                                    });
+                            axios.delete(this.endpoint)
+                                .then(res => {
+                                    $(this.$el).fadeOut(500, () => {
+                                        this.$toast.success(res.data.message, "Success", {timeout: 3000});
+                                    })
+                                });
 
 
                             instance.hide({transitionOut: 'fadeOut'}, toast, 'button');
